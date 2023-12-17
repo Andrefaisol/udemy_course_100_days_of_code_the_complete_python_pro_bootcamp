15fbd8d087ed84a080c680e45c96afebba5e1dd5
Andrefaisol <andrefaisol0002@gmail.com>
2023-12-17 19:01:09 +0700

    new line
    checking

--- Day 1 project variable.py
+++ Day 1 project variable.py
@@ -6,3 +6,6 @@ pet_name = str(input('What is your first name pet:\n'))
 villain_name = str(input('what is your favorite villain name:\n'))
 
 print('Your band name is ' + pet_name + " " + villain_name)
+
+
+#first day, it really interesting
