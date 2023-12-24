520757e8790dfc04d0e287fbaf0e2939af3b80db
Andrefaisol <andrefaisol0002@gmail.com>
2023-12-24 08:43:38 +0700

    latihan 9 added
    nesting dictionary and list

---
+++ latihan 9.py
@@ -0,0 +1,30 @@
+# programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.",
+#                           "Function": "A piece of code that you can easily call over and over again."}
+#
+# print(programming_dictionary["Bug"])
+# print("---------------------------------------------------------------------------")
+# programming_dictionary["Loop"] = "The action of doing something over and over again."
+# print(programming_dictionary)
+# print("---------------------------------------------------------------------------")
+# empty_dict = {}
+# empty_dict["Eat"] = "Action of consuming food"
+# print(empty_dict)
+# print("----------------------------------------------------------------------------")
+# programming_dictionary["Bug"] = "An error of program wether it's expected or not expected."
+# print(programming_dictionary["Bug"])
+#
+# for key in programming_dictionary:
+#     print(key + ": " + programming_dictionary[key])
+
+travel_log = [
+    {
+        "country": "France",
+        "cities_visited": ["Paris", "Lille", "Dijon",],
+        "total_visited": 32
+    },
+    {
+        "country": "Germany",
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart",],
+        "total_visited": 27},
+]
+print(travel_log)
