683c4c7319128cad13f635de5c21bc4f8ef1646a
Andrefaisol <andrefaisol0002@gmail.com>
2024-01-04 12:35:39 +0700

    add 'back' option

--- Day 15 Project Coffe Machine.py
+++ Day 15 Project Coffe Machine.py
@@ -8,31 +8,31 @@ def resource_report(water, coffe, milk):
 
 
 def resource_check(water, coffe, milk, command):
-    global user_command
+    global perintah
     if command == "espresso":
         if water < 50 or coffe < 18:
             print("=========================================================")
             print("Water or Coffe not enough, please add some water or coffe")
             print("=========================================================")
-            user_command = ""
+            perintah = ""
     elif command == "latte":
         if water < 200 or coffe < 24 or milk < 150:
             print("=====================================================================")
             print("Water, Coffe or milk not enough, please add some water, coffe or milk")
             print("=====================================================================")
-            user_command = ""
+            perintah = ""
     elif command == "americano":
         if water < 200 or coffe < 18:
             print("=======================================================")
             print("Water, Coffe not enough, please add some water or coffe")
             print("=======================================================")
-            user_command = ""
+            perintah = ""
     elif command == "latte":
         if water < 250 or coffe < 24 or milk < 100:
             print("=====================================================================")
             print("Water, Coffe or milk not enough, please add some water, coffe or milk")
             print("=====================================================================")
-            user_command = ""
+            perintah = ""
     else:
         return water, coffe, milk
     return water, coffe, milk
@@ -49,7 +49,6 @@ def converter(w, x, y, z):
 
 
 def insert_money():
-    global money
     penny = ""
     nickel = ""
     dime = ""
@@ -62,18 +61,15 @@ def insert_money():
         dime = input("How many dime do you insert: ")
     while not quarter.isdigit():
         quarter = input("How many quarter do you insert: ")
-    money = cent_to_dollar(converter(w=int(penny), x=int(nickel), y=int(dime), z=int(quarter)))
-    return money
+    total = cent_to_dollar(converter(w=int(penny), x=int(nickel), y=int(dime), z=int(quarter)))
+    return total
 
 
-water_capacity = 1000
-coffe_capacity = 200
-milk_capacity = 500
-machine_coffe_on = True
-while machine_coffe_on:
+def coffe_on_ready():
     money = 0
     user_command = ""
-    print("Coffe machine on please order your coffe")
+    back_to = "a"
+    print("Coffe machine ready, please order your coffe")
     while user_command not in MENU:
         if user_command == "report":
             resource_report(water=water_capacity, coffe=coffe_capacity, milk=milk_capacity)
@@ -85,39 +81,59 @@ while machine_coffe_on:
                              "What would you like?: ").lower()
         if user_command in MENU:
             resource_check(water=water_capacity, coffe=coffe_capacity, milk=milk_capacity, command=user_command)
+    print("================================================================")
     if user_command == "turn off":
         print("Goodbye")
-        break
+        return money, user_command
     elif user_command in MENU:
         while money < PRICE[user_command]:
-            print(f"Espresso price is ${PRICE[user_command]}, please insert your amount money to proceed")
-            insert_money()
+            print(f"{user_command} price is ${PRICE[user_command]}, please insert your amount money to proceed")
+            money = insert_money()
             if money < PRICE[user_command]:
-                print(f"Espresso price is ${PRICE[user_command]} your money is not enough")
-                print(f"You get refund ${money}")
+                print(f"{user_command} price is ${PRICE[user_command]} your money is not enough")
+                print("You get refund ${:.2f}".format(money))
+                print("================================================================")
                 money = 0
+                while back_to != "back" and back_to != "":
+                    back_to = input("Would you like to reinsert money or change order?\n"
+                                    "type 'back' to change order or press enter to reinsert money: ").lower()
+                if back_to == "back":
+                    break
+    if back_to == "back":
+        money, user_command = coffe_on_ready()
+    return money, user_command
 
-        if user_command == "espresso":
-            water_capacity -= 50
-            coffe_capacity -= 18
-        elif user_command == "americano":
-            water_capacity -= 200
-            coffe_capacity -= 18
-        elif user_command == "latte":
-            water_capacity -= 200
-            coffe_capacity -= 24
-            milk_capacity -= 150
-        elif user_command == "cappuccino":
-            water_capacity -= 250
-            coffe_capacity -= 24
-            milk_capacity -= 100
+
+water_capacity = 1000
+coffe_capacity = 200
+milk_capacity = 500
+machine_coffe_on = True
+while machine_coffe_on:
+    uang, perintah = coffe_on_ready()
+
+    if perintah == "turn off":
+        break
+    elif perintah == "espresso":
+        water_capacity -= 50
+        coffe_capacity -= 18
+    elif perintah == "americano":
+        water_capacity -= 200
+        coffe_capacity -= 18
+    elif perintah == "latte":
+        water_capacity -= 200
+        coffe_capacity -= 24
+        milk_capacity -= 150
+    elif perintah == "cappuccino":
+        water_capacity -= 250
+        coffe_capacity -= 24
+        milk_capacity -= 100
     print("================================================================")
-    print(f"Here your {user_command}!")
+    print(f"Here your {perintah}!")
     print("================================================================")
-    if money > PRICE[user_command]:
-        print(f"Your total money is ${money}")
-        money -= PRICE[user_command]
-        print("Here your change ${:.2f}".format(money))
+    if uang > PRICE[perintah]:
+        print(f"Your total money is ${uang}")
+        uang -= PRICE[perintah]
+        print("Here your change ${:.2f}".format(uang))
     print("================================================================")
     resource_report(water=water_capacity, coffe=coffe_capacity, milk=milk_capacity)
     print("================================================================")
