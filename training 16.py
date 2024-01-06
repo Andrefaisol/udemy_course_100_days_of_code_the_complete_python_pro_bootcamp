6f16619a5675d46fe438eab5e85e8692c7a404c8
Andrefaisol <andrefaisol0002@gmail.com>
2024-01-06 12:04:47 +0700

    update

--- training 16.py
+++ training 16.py
@@ -1,12 +1,27 @@
-from prettytable import prettytable
+from prettytable import PrettyTable
 from turtle import Turtle
 from turtle import Screen
 
-slowly = Turtle()
-slowly.shape("turtle")
-slowly.color("green")
-slowly.forward(100)
+# slowly = Turtle()
+# slowly.shape("turtle")
+# slowly.color("green")
+# slowly.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+#
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+table = PrettyTable()
+table.field_names = ["Pokemon", "Type"]
+table.add_rows(
+    [
+        ["Charmander", "Fire"],
+        ["Tortoise", "Water"],
+        ["Bulbasaur", "Grass"],
+    ]
+)
+
+table.align["Pokemon"] = "l"
+table.align["Type"] = "l"
+print(table)
