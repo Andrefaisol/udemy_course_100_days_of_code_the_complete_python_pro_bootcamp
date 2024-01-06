9c743f6692163fa329b9f33ddbbb7baa0e596be2
Andrefaisol <andrefaisol0002@gmail.com>
2024-01-06 07:44:49 +0700

    day 16

---
+++ training 16.py
@@ -0,0 +1,12 @@
+from prettytable import prettytable
+from turtle import Turtle
+from turtle import Screen
+
+slowly = Turtle()
+slowly.shape("turtle")
+slowly.color("green")
+slowly.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
